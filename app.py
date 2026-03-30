bd5f5458f418eab84f771a748f30a04a32142f7d
Cohorte 5 <jjvalela0203@gmail.com>
2026-03-30 07:41:33 -0500

    feat: first version of functions

--- app.py
+++ app.py
@@ -3,13 +3,12 @@
 import os
 import functions
 
+list=[]
+
 def main_menu():
     """
     """
-    state=True
-
-    while state:
-        logo=r"""
+    logo=r"""
      __ _             _            _           __            _     _            
     / _\ |_ _   _  __| | ___ _ __ | |_ ___    /__\ ___  __ _(_)___| |_ ___ _ __ 
     \ \| __| | | |/ _` |/ _ \ '_ \| __/ __|  / \/// _ \/ _` | / __| __/ _ \ '__|
@@ -18,4 +17,141 @@ def main_menu():
                                                        |___/                    """
     
     print(logo)
+    print("\n", "="*30, "MENU", "="*30)
+    print("1) Add student")
+    print("2) Show list of students")
+    print("3) Search student")
+    print("4) Update student info")
+    print("5) Delete student")
+    print("6) Save list")
+    print("7) Load list")
+    print("8) Exit")
+
+menu= True
+
+while menu:
+    main_menu()
+
+    valid= True
     
+    while valid:
+        try:
+            option=int(input("Select an option:"))
+            if 1<= option <= 6:
+                valid= False
+            else:
+                functions.clear_screen()
+                print("\n", "="*30, "MENU", "="*30)
+                print("Invalid option, Please enter a valid number")
+                input("Press enter to continue")
+                functions.clear_screen()
+        except ValueError:
+            print("\n", "="*30, "MENU", "="*30)
+            print("Invalid option. please enter a number")
+            input("Press enter to continue")
+            functions.clear_screen()
+
+    if option == 1:
+        functions.clear_screen()
+        
+        print("\n", "="*30, "ADD STUDENT", "="*30)
+        
+        #The program ask for the data of the student
+        
+        try:
+            id=int(input("Enter student ID:"))
+            if id < 0:
+                print("Invalid ID")
+                id=int(input("Enter student ID:"))
+        except ValueError:
+            print("Invalid ID")
+            id=int(input("Enter student ID:"))
+        
+        name=input("Enter student name:")
+        
+        try:
+            age=int(input("Enter student age:"))
+            while age <= 0:
+                print("Invalid age")
+                age=int(input("Enter student age:"))
+        except ValueError:
+            print("Invalid age")
+            age=int(input("Enter student age:"))
+        
+        program=input("Enter student program:")
+        
+        state=input("Enter student state(A for active and I for inactive):").lower()
+        while state != "a" and state != "i":
+            print("Invalid state")
+            state= input("Enter student state(A for active and I for inactive):").lower()
+
+        functions.add_student(list, id, name, age, program, state)
+
+        functions.clear_screen()
+
+
+    elif option == 2:
+        functions.clear_screen()
+        print("\n", "="*30, "LIST OF STUDENTS", "="*30)
+        functions.show_list(list)
+
+
+    elif option == 3:
+        functions.clear_screen()
+        functions.Search_student(list, id, name, age, program, state)
+
+    elif option == 4:
+        functions.clear_screen()
+        print("\n", "="*30, "UPDATE STUDENT", "="*30)
+        
+        student=input("Enter student name:")
+
+        try:
+            new_name=input("Enter new student name (Enter to skip):")
+            
+            new_age=int(input("Enter new student age (Enter to skip):"))
+            while new_age <= 0:
+                print("Invalid age")
+                new_age
+
+            new_program=input("Enter new student program (Enter to skip):")
+
+            new_state=input("Enter new student state(A for active and I for inactive) (Enter to skip):").lower()
+            while new_state != "a" and new_state != "i":
+                print("Invalid state")
+                new_state=input("Enter new student state(A for active and I for inactive) (Enter to skip):").lower()
+        
+        except ValueError:
+            print("Invalid data")
+            input("Press enter to continue")
+            functions.clear_screen()
+            
+    elif option == 5:
+        functions.clear_screen()
+        print("\n", "="*30, "DELETE STUDENT", "="*30)
+
+    elif option == 6:
+        functions.clear_screen()
+        print("\n", "="*30, "SAVE LIST", "="*30)
+    
+    elif option == 7:
+        functions.clear_screen()
+        print("\n", "="*30, "LOAD LIST", "="*30)
+
+    elif option == 8:
+        functions.clear_screen()
+        print("\n", "="*30, "EXIT", "="*30)
+        print("Goodbye!")
+        input("Press enter to continue")
+        functions.clear_screen()
+        menu= False
+
+
+
+
+
+            
+
+
+
+
--- functions.py
+++ functions.py
@@ -42,13 +42,149 @@ def show_list(list):
     else:  #On the other hand if the list conta
         for student in list:
             print("\nStudent ID:", student["id"],
-                  "Name:", student["name"],
-                  "Age:", student["age"],
-                  "Program:", student["program"],
-                  "State:", student["state"])
+                  "| Name:", student["name"],
+                  "| Age:", student["age"],
+                  "| Program:", student["program"],
+                  "| State:", student["state"])
         
     input("Press enter to continue")
     clear_screen()
+
+def Search_student(list, id, name, age, program, state):
+    """
+    This function search students in the list
+    For one characteristic, and print the student found info
+    if no student is find, print a message of not found
+    """
+    submenu= True
+
+    while submenu:
+        print("\n", "="*30, "SEARCH STUDENT", "="*30)
+        print("1) Search by ID")
+        print("2) Search by name")
+        print("3) Search by age")
+        print("4) Search by program")
+        print("5) Search by state")
+        print("6) Back to main menu")
+
+
+        selection=int(input("Select an option:"))
+
+        if selection == 1:
+            clear_screen()
+            print("\n", "="*30, "SEARCH STUDENT", "="*30)
+            id=int(input("Enter student ID:"))
+            for student in list:
+                if student["id"] == id:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("\nStudent Found:", student)
+                    input("Press enter to continue")
+                    clear_screen()
+                else:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("Student not found")
+                    input("Press enter to continue")
+                    clear_screen()
+        
+        elif selection == 2:
+            name=input("Enter student name:")
+            for student in list:
+                if student["name"].lower() == name.lower():
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("\nStudent Found:", student)
+                    input("Press enter to continue")
+                    clear_screen()
+                else:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("Student not found")
+                    input("Press enter to continue")
+                    clear_screen()
+        
+        elif selection == 3:
+            age=int(input("Enter student age:"))
+            for student in list:
+                if student["age"] == age:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("\nStudent Found:", {student})
+                    input("Press enter to continue")
+                    clear_screen()
+                else:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("Student not found")
+                    input("Press enter to continue")
+                    clear_screen()
+        
+        elif selection == 4:
+            program=input("Enter student program:")
+            for student in list:
+                if student["program"].lower() == program.lower():
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("\nStudent Found:", {student})
+                    input("Press enter to continue")
+                    clear_screen()
+                else:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("Student not found")
+                    input("Press enter to continue")
+                    clear_screen()
+
+        elif selection == 5:
+            state=input("Enter student state(A for active and I for inactive):").lower()
+            for student in list:
+                if student["state"].lower() == state.lower():
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("\nStudent Found:", {student})
+                    input("Press enter to continue")
+                    clear_screen()
+                else:
+                    clear_screen()
+                    print("\n", "="*30, "SEARCH STUDENT", "="*30)
+                    print("Student not found")
+                    input("Press enter to continue")
+                    clear_screen()
+        
+        elif selection == 6:
+            submenu= False
+            clear_screen()
+
+        else:
+            print("Invalid option")
+            input("Press enter to continue")
+            clear_screen()
+
+def update_student(list, id, name, age, program, state):
+    """
+    """
+    student=Search_student(list, name)
+    
+    if student is None:
+        print("\n", "="*30, "UPDATE STUDENT", "="*30)
+        print("Student not found")
+        input("Press enter to continue")
+        clear_screen()
+        return
+    
+
+
+
+
+
+
+            
+            
+        
+
+
+
             
 
 
